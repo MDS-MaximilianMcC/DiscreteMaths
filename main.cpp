a9ff057cbe23f7660d91b0e7df7806f4214b3a4e
MDS-MaximilianMcC <maximilian.mccarthy@mds.ac.nz>
2026-05-28 09:53:30 +1200

    fancy testing method

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include "discretemaths.h"
 
-void test()
+bool test(float enteredValue, float expectedValue, int& totalTests)
 {
+	totalTests++;
+	bool passed = enteredValue == expectedValue;
 
+	// Show a little error if we failed only
+	if (passed == false)
+	{
+		std::cout << "Test " << totalTests << " failed! Expected " << expectedValue << " but got " << enteredValue << "!" << std::endl;
+	}
+
+	return passed;
 }
 
 int main()
 {
 	std::cout << "Hello World!\n";
 
+	int totalTests = 0;
 	int testsPassed = 0;
 
 	// TODO: Make proper testing class
+	testsPassed += test(Magnitude(-5), 5, totalTests);
+	testsPassed += test(Magnitude(-0), 0, totalTests);
+	testsPassed += test(Magnitude(0), 0, totalTests);
+	testsPassed += test(Magnitude(5), 5, totalTests);
 
-	std::cout << Magnitude(-5) << std::endl;
-	std::cout << Magnitude(0) << std::endl;
-	std::cout << Magnitude(-0) << std::endl;
-	std::cout << Magnitude(5) << std::endl;
+	std::cout << "Passed " << testsPassed << "/" << totalTests << " tests!" << std::endl;
 
 	// Stop the program from closing
 	std::cin.get();
